b68a5e94a9fd497d2a9b07cb6f8e0cd7f200687c
abc <abc@gmail.com>
2022-08-11 17:59:15 +0300

    v1

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector(".imgg");
+let container = document.querySelector(".container");
+
+function phones(phone){
+    img.src = phone;
+}
+
+
+function colors(color){
+    container.style.background = color;
+}
